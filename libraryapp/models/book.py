9d9c27a861ccd48b882084d37bb36d37d93f5efe
Lauren Riddle <lari7132@gmail.com>
2020-02-05 15:55:39 -0600

    completed chapter 2

---
+++ libraryapp/models/book.py
@@ -0,0 +1,15 @@
+from django.db import models
+from .librarian import Librarian
+from .library import Library
+
+
+class Book(models.Model):
+
+    title = models.CharField(max_length = 50)
+    ISBN = models.CharField(max_length=100)
+    author = models.CharField(max_length=50)
+    year_published = models.IntegerField()
+    location = models.ForeignKey(Library, on_delete=models.CASCADE)    
+    librarian = models.ForeignKey(Librarian, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title

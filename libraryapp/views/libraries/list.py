513fbdf37d200fa1da9763a8f0472e9933ed67ed
Lauren Riddle <lari7132@gmail.com>
2020-02-07 19:33:40 -0600

    working on getting library to show with books

--- libraryapp/views/libraries/list.py
+++ libraryapp/views/libraries/list.py
@@ -3,6 +3,29 @@ from django.shortcuts import render, redirect, reverse
 from libraryapp.models import Library, Book, model_factory
 from ..connection import Connection
 from django.contrib.auth.decorators import login_required
+def create_library(cursor, row):
+    _row = sqlite3.Row(cursor, row)
+
+    library = Library()
+    library.id = _row["id"]
+    library.title = _row["title"]
+    library.address = _row["address"]
+
+    # Note: You are adding a blank books list to the library object
+    # This list will be populated later (see below)
+    library.books = []
+
+    book = Book()
+    book.id = _row["book_id"]
+    book.title = _row["book_title"]
+    book.author = _row["author"]
+    book.ISBN = _row["ISBN"]
+    book.year_published = _row["year_published"]
+
+    # Return a tuple containing the library and the
+    # book built from the data in the current row of
+    # the data set
+    return (library, book)
 
 @login_required
 def library_list(request):
@@ -26,7 +49,7 @@ def library_list(request):
             """)
 
             libraries = db_cursor.fetchall()
-            # Start with an empty dictionary
+        # Start with an empty dictionary
             library_groups = {}
 
             # Iterate the list of tuples
@@ -35,21 +58,21 @@ def library_list(request):
                 # If the dictionary does have a key of the current
                 # library's `id` value, add the key and set the value
                 # to the current library
+                print(library, book)
                 if library.id not in library_groups:
                     library_groups[library.id] = library
                     library_groups[library.id].books.append(book)
-
+                    print(library_groups[library.id].books)
                 # If the key does exist, just append the current
                 # book to the list of books for the current library
                 else:
                     library_groups[library.id].books.append(book)
                 print(library_groups)
-
-
-        template = 'libraries/list.html'
-        context = {
-            'all_libraries': library_groups
-        }
+            print("PLEASE", library_groups)
+            template = 'libraries/list.html'
+            context = {
+                'all_libraries': library_groups
+            }
 
 
         return render(request, template, context)
@@ -69,26 +92,3 @@ def library_list(request):
             (form_data['title'], form_data['address']))
 
         return redirect(reverse('libraryapp:libraries'))
-def create_library(cursor, row):
-    _row = sqlite3.Row(cursor, row)
-
-    library = Library()
-    library.id = _row["id"]
-    library.title = _row["title"]
-    library.address = _row["address"]
-
-    # Note: You are adding a blank books list to the library object
-    # This list will be populated later (see below)
-    library.books = []
-
-    book = Book()
-    book.id = _row["book_id"]
-    book.title = _row["book_title"]
-    book.author = _row["author"]
-    book.isbn = _row["isbn"]
-    book.year_published = _row["year_published"]
-
-    # Return a tuple containing the library and the
-    # book built from the data in the current row of
-    # the data set
-    return (library, book,)
